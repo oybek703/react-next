e127c70ffb0a7a2bd0b9dc35048b2bca08c38962
Yoriqulov Oybek <hhoybek@gmail.com>
2022-09-30 19:19:36 +0500

    withLayout HOC added

--- layout/Layout/index.tsx
+++ layout/Layout/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {FunctionComponent} from 'react'
 import {LayoutProps} from './Layout.props'
 import {Header} from '../Header'
 import {Sidebar} from '../Sidebar'
 import {Footer} from '../Footer'
 
-export const Layout = ({children}: LayoutProps): JSX.Element => {
+const Layout = ({children}: LayoutProps): JSX.Element => {
     return <>
         <Header/>
         <div>
@@ -15,5 +15,13 @@ export const Layout = ({children}: LayoutProps): JSX.Element => {
         </div>
         <Footer/>
     </>
+}
+
+export function withLayout<T extends Record<string, unknown>>(Component: FunctionComponent<T>) {
+    return function WithLayoutWrapper(props: T): JSX.Element {
+        return <Layout>
+            <Component {...props} />
+        </Layout>
+    }
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type {NextPage} from 'next'
 import {Button, HTag, PTag, Rating, Tag} from '../components'
 import {useState} from 'react'
-import {Layout} from '../layout/Layout'
+import {withLayout} from '../layout/Layout'
 
 const Home: NextPage = () => {
     const [rating, setRating] = useState<number>(2)
     return (
-        <Layout>
+        <>
             <HTag tag="h3">This is HTag children</HTag>
             <Button arrow="right" appereance="primary">Click</Button>
             <Button arrow="down" appereance="ghost">Click</Button>
@@ -19,8 +19,8 @@ const Home: NextPage = () => {
             <Tag color='green'>Ghost</Tag>
             <Tag size='small' color='primary'>Ghost</Tag>
             <Rating editable setRating={setRating} rating={rating}/>
-        </Layout>
+        </>
     )
 }
 
-export default Home
+export default withLayout(Home)
