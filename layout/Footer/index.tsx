bce4dda9423ce4d6f09305deef2c4f6ec5828cc4
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-01 23:14:24 +0500

    footer layout basic self implementation done

--- layout/Footer/index.tsx
+++ layout/Footer/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import {FooterProps} from './Footer.props'
 import classNames from 'classnames'
+import styles from './Footer.module.css'
+import { format } from 'date-fns'
 
 export const Footer = ({ children, className, ...props}: FooterProps): JSX.Element => {
-    return <div className={classNames(className)}>
-        Footer
+    return <div className={classNames(className, styles.footer)}>
+        <p className="copyright">
+            OwlTop © 2021 - {format(new Date(), 'yyyy')} Все права защищены
+        </p>
+        <p className="agreement">
+            Пользовательское соглашение
+        </p>
+        <p className="terms">
+            Политика конфиденциальности
+        </p>
     </div>
 }
 
