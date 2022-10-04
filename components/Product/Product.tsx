73a26e8f73d78dbd8ba1a2af925709c665da6246
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-04 20:05:27 +0500

    Product component styling stuck

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -6,6 +6,7 @@ import {Card} from '../Card/Card'
 import {Rating} from '../Rating/Rating'
 import {Tag} from '../Tag/Tag'
 import {Button} from '../Button/Button'
+import {priceRu} from '../../helpers'
 
 export const Product = ({product}: ProductProps): JSX.Element => {
     return <Card className={classNames(styles.product)}>
@@ -15,13 +16,18 @@ export const Product = ({product}: ProductProps): JSX.Element => {
                 alt={product.title}/>
         </div>
         <div className={styles.title}>{product.title}</div>
-        <div className={styles.price}>{product.price}</div>
-        <div className={styles.credit}>{product.credit}</div>
+        <div className={styles.price}>
+            {priceRu(product.price)}
+            {/*{product.oldPrice && <Tag color='green' size='small' className={styles.oldPrice}>{priceRu(product.price - product.oldPrice)}</Tag>}*/}
+        </div>
+        <div className={styles.credit}>
+            {priceRu(product.credit)}/<span className={styles.month}>мес</span>
+        </div>
         <div className={styles.rating}>
             <Rating rating={product.reviewAvg ?? product.initialRating}/>
         </div>
         <div className={styles.tags}>
-            {product.categories.map(category => <Tag key={category} color="ghost">{category}</Tag>)}
+            {product.categories.map(category => <Tag size='small' key={category} color="ghost">{category}</Tag>)}
         </div>
         <div className={styles.priceTitle}>цена</div>
         <div className={styles.creditTitle}>кредит</div>
