e00024159760d25e39f99f3578db804858b19f9f
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-04 19:02:46 +0500

    Product page basic styling done with css grid

--- pageComponents/TopPage/index.tsx
+++ pageComponents/TopPage/index.tsx
@@ -1,7 +1,7 @@
 import React, {useReducer} from 'react'
 import {TopPageProps} from './TopPage.props'
 import styles from './TopPage.module.css'
-import {Advantages, HhData, HTag, Sort, Tag} from '../../components'
+import {Advantages, HhData, HTag, Product, Sort, Tag} from '../../components'
 import {TopLevelCategory} from '../../interfaces/page.interface'
 import {SortEnum} from '../../components/Sort/Sort.props'
 import {sortReducer} from '../../components/Sort/sortReducer'
@@ -22,9 +22,9 @@ export const TopPage = ({products, page, firstCategory}: TopPageProps): JSX.Elem
             <HTag tag={'h1'}>{page.title}</HTag>
             <Tag size="medium" color="grey">{products?.length}</Tag>
             <Sort sort={sort} setSort={setSort}/>
-            <div>
-                {sortedProducts?.map(p => <div className={styles.courseTitle} key={p._id}>{p.title}</div>)}
-            </div>
+        </div>
+        <div>
+            {sortedProducts?.map(p => <Product product={p} key={p._id}/>)}
         </div>
         <div className={styles.hhTitle}>
             <HTag tag='h2'>Вакансии - {page.category}</HTag>
