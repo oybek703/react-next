102059e74d3a81bf31bf545fcc9ec72d155069cc
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-05 11:53:20 +0500

    Product reviews formatting done

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -6,7 +6,7 @@ import {Card} from '../Card/Card'
 import {Rating} from '../Rating/Rating'
 import {Tag} from '../Tag/Tag'
 import {Button} from '../Button/Button'
-import {priceRu} from '../../helpers'
+import {decOfNum, priceRu} from '../../helpers'
 import {Divider} from '../Divider/Divider'
 
 export const Product = ({product}: ProductProps): JSX.Element => {
@@ -34,7 +34,10 @@ export const Product = ({product}: ProductProps): JSX.Element => {
         </div>
         <div className={styles.priceTitle}>цена</div>
         <div className={styles.creditTitle}>кредит</div>
-        <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+        <div className={styles.rateTitle}>
+            {product.reviewCount} &nbsp;
+            {decOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+        </div>
         <Divider className={styles.hr}/>
         <div className={styles.description}>{product.description}</div>
         <div className={styles.feature}>фичи</div>
