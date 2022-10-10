c4e6cec0432c4cc11ef9cbab8ae17b8ca6caa497
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-10 12:43:43 +0500

    color accessibility updated and keyboard acc start

--- layout/Menu/index.tsx
+++ layout/Menu/index.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, KeyboardEvent} from 'react'
 import {AppContext} from '../../context/app.context'
 import {FirstLevelMenuItem, PageItem} from '../../interfaces/menu.interface'
 import styles from './Menu.module.css'
@@ -6,12 +6,11 @@ import classNames from 'classnames'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 import {firstLevelMenu} from '../../helpers'
-import { motion } from 'framer-motion'
+import {motion} from 'framer-motion'
 
 export const Menu = (): JSX.Element => {
     const {menu, firstCategory, setMenu} = useContext(AppContext)
     const router = useRouter()
-
     const variants = {
         visible: {
             marginBottom: 20,
@@ -25,7 +24,6 @@ export const Menu = (): JSX.Element => {
             marginBottom: 0
         }
     }
-
     const variantsChildren = {
         visible: {
             opacity: 1,
@@ -36,8 +34,6 @@ export const Menu = (): JSX.Element => {
             height: 0
         }
     }
-
-
     const setSecondLevel = (secondCategory: string) => {
         setMenu && setMenu(menu.map(menuItem => {
             if (menuItem._id.secondCategory === secondCategory) {
@@ -65,6 +61,14 @@ export const Menu = (): JSX.Element => {
             </>
         )
     }
+
+    const handleSecondLevelKeyDown = (event: KeyboardEvent<HTMLDivElement>, secondCategory: string) => {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            event.preventDefault()
+            setSecondLevel(secondCategory)
+        }
+    }
+
     const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
         return (
             <div className={styles.secondBlock}>
@@ -74,13 +78,17 @@ export const Menu = (): JSX.Element => {
                     }
                     return (
                         <div key={m._id.secondCategory}>
-                            <div onClick={() => setSecondLevel(m._id.secondCategory)}
+                            <div tabIndex={0}
+                                 onKeyDown={
+                                     (event) => handleSecondLevelKeyDown(event, m._id.secondCategory)
+                                 }
+                                 onClick={() => setSecondLevel(m._id.secondCategory)}
                                  className={styles.secondLevel}>{m._id.secondCategory}</div>
                             <motion.div className={classNames(styles.secondLevelBlock)}
                                         variants={variants}
                                         initial={m.opened ? 'visible' : 'hidden'}
                                         animate={m.opened ? 'visible' : 'hidden'} layout>
-                                {buildThirdLevel(m.pages, menuItem.route)}
+                                {buildThirdLevel(m.pages, menuItem.route, m.opened ?? false)}
                             </motion.div>
                         </div>
                     )
@@ -88,13 +96,13 @@ export const Menu = (): JSX.Element => {
             </div>
         )
     }
-    const buildThirdLevel = (pages: PageItem[], route: string) => {
+    const buildThirdLevel = (pages: PageItem[], route: string, isRouteOpen: boolean) => {
         return (
             <>
                 {pages.map(page =>
                     <motion.div variants={variantsChildren} key={page._id}>
                         <Link href={`/${route}/${page.alias}`}>
-                            <a className={classNames(styles.thirdLevel, {
+                            <a tabIndex={isRouteOpen ? 0 : -1} className={classNames(styles.thirdLevel, {
                                 [styles.thirdLevelActive]: router.asPath.split('/')[2] === page.alias
                             })}>
                                 {page.category}
--- layout/Sidebar/index.tsx
+++ layout/Sidebar/index.tsx
@@ -3,14 +3,14 @@ import {SidebarProps} from './Sidebar.props'
 import classNames from 'classnames'
 import {Menu} from '../Menu'
 import styles from './Sidebar.module.css'
-import Logo from '../logo.svg'
+import LogoIcon from '../logo.svg'
 import Link from 'next/link'
 import {Search} from '../../components'
 
 export const Sidebar = ({ className, ...props}: SidebarProps): JSX.Element => {
     return <div className={classNames(className, styles.sidebar)} {...props}>
         <Link href='/'>
-            <a><Logo className={styles.logo}/></a>
+            <a><LogoIcon className={styles.logo}/></a>
         </Link>
         <Search/>
         <Menu/>
