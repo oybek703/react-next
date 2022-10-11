96bc0c92f4c7c09cdf55a43e3a8c73df81ff906f
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-11 15:20:28 +0500

    yandex metrika added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,21 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {Fragment} from 'react'
 import Head from 'next/head'
+import ym, {YMInitializer} from 'react-yandex-metrika'
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({Component, pageProps, router}: AppProps) {
+    router.events && router.events.on('routeChangeComplete', function (url: string) {
+        if (typeof window !== 'undefined') {
+            ym('hit', url)
+        }
+    })
     return <Fragment>
         <Head>
             <title>YOTopC - informational App</title>
             <meta property='og:locale' content='ru_RU'/>
+            <link rel="preconnect" href="https://mc.yandex.ru"/>
         </Head>
+        <YMInitializer accounts={[]} options={{webvisor: true, defer: true}} version='2'    />
         <Component {...pageProps} />
     </Fragment>
 }
