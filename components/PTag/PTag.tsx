2404dd19d9b731810d4172361af2e2df7d7cf1d1
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-11 15:30:17 +0500

    husky added

--- components/PTag/PTag.tsx
+++ components/PTag/PTag.tsx
@@ -4,7 +4,7 @@ import styles from './PTag.module.css'
 import classNames from 'classnames'
 
 export const PTag = ({size = 'medium', className, children, ...props}: PTagProps): JSX.Element => {
-    return <p className={classNames(styles.p, className, {
+    return <p className={classNames(styles.pTag, className, {
         [styles.small]: size === 'small',
         [styles.medium]: size === 'medium',
         [styles.large]: size === 'large'
